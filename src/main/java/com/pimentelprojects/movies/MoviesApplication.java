60a7bdc1f42e104e6922b14ed62b1d981f932b93
Omar Pimentel <pimentelomar6@gmail.com>
2023-03-04 23:44:47 -0400

    Solucionando problemas de CORS

--- src/main/java/com/pimentelprojects/movies/MoviesApplication.java
+++ src/main/java/com/pimentelprojects/movies/MoviesApplication.java
@@ -2,12 +2,28 @@ package com.pimentelprojects.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("api/v1/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("*")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
