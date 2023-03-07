3ab7951fe2c7e45cdeabd4e36d767ab2a6b9de94
Omar Pimentel <pimentelomar6@gmail.com>
2023-03-06 22:06:49 -0400

    Solucionando problemas de CORS

--- src/main/java/com/pimentelprojects/movies/MoviesApplication.java
+++ src/main/java/com/pimentelprojects/movies/MoviesApplication.java
@@ -19,9 +19,7 @@ public class MoviesApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/api/v1/**")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("*")
-						.allowedHeaders("*");
+						.allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
